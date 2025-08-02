55e37679e43e2c662141c881989603ce61efb78d
Aakash52thade <117812701+Aakash52thade@users.noreply.github.com>
2025-08-02 13:57:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+function domLoaded() {
+	let str = document.createElement('p');
+	str.textContent = "Dom load Success";
+	document.body.appendChild(str);
+}
+
+document.addEventListener("DOMContentLoaded", domLoaded);
